d475b0b199bdf87c78a663cd5326e5da7aa26b5b
kohkubo <kohkubo@student.42tokyo.jp>
2021-09-27 17:59:40 +0900

    不要な変数を消去

--- includes/philo.h
+++ includes/philo.h
@@ -20,18 +20,10 @@ enum	e_data
 	EC,
 	ANK
 };
-typedef enum e_status
-{
-	GRAB = 0,
-	EAT,
-	THINK,
-	SLEEP,
-	DIE
-}				t_status;
+
 typedef struct s_philo
 {
 	int			id;
-	t_status	status;
 	int			eat_count;
 	int64_t		last_eat_time;
 	int64_t		think_time;
@@ -43,13 +35,9 @@ typedef struct s_data
 	int				main[ANK];
 	t_philo			philos[PN_MAX];
 	pthread_t		threads[PN_MAX];
-	pthread_t		monitors[PN_MAX];
 	pthread_mutex_t	forks[PN_MAX];
 	pthread_mutex_t	print_mutex;
 	bool			dead_flg;
-	struct timeval	die_to_time;
-	struct timeval	start_time;
-	struct timeval	now_time;
 }				t_data;
 extern t_data	g_p;
 
