88c5aeea88a87e51f36f8c3d2bdfa5ca0e1ab52b
kohkubo <kohkubo@student.42tokyo.jp>
2021-09-28 15:08:43 +0900

    update testcaes

--- philo/eat_time.sh
+++ philo/eat_time.sh
@@ -14,4 +14,12 @@ done
 
 
 echo "Testing 2 ================"
-./philo 200 410 200 200 | awk '{print $1}' | tee act | sort -n > exp; diff -u exp act || echo "OK"
+./philo 200 403 200 200 | awk '{print substr($1, length($1) - 12, 13)}' | tee act | sort -n > exp;
+gsed -i '$d' act;
+gsed -i '1d' exp;
+diff -u exp act
+if [ $? -eq 0 ]; then
+	echo "Test 2 passed"
+else
+	echo "Test 2 failed"
+fi
